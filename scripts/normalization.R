eb733b2ba9e70b0224a256404a86c8fe0dcab9e9
programLyrique <pierre.donat.bouillud@gmail.com>
2021-01-28 12:44:38 +0100

    Normalization: VAr absorbing

--- scripts/normalization.R
+++ scripts/normalization.R
@@ -15,11 +15,11 @@ str_op <- c("paste", "paste0", "str_c")
 comp_op <- c("<", ">", "<=", ">=", "==", "!=")
 bool_op <- c("&", "&&", "|", "||", "!")
 
-canonic_expr <- function(exp, keep.names = FALSE) {
+canonic_expr <- function(exp, with.names = FALSE) {
     if (is.call(exp)) {
         function_name <- exp[[1]]
         function_args <- exp[-1]
-        res <- map(function_args, canonic_expr) # TODO: rather directly compute canonic_expr on function_args?
+        res <- map(function_args, canonic_expr, with.names) # TODO: rather directly compute canonic_expr on function_args?
         # Function_name can be larger than one is a function with a namespace stats::model or another higher-order function
         # For instance, (function(x, y) x + y)(3, 4)
         if (length(function_name) == 1 && as.character(function_name) == "(") {
@@ -45,27 +45,80 @@ canonic_expr <- function(exp, keep.names = FALSE) {
         })) {
             return("BOOL")
         }
-        else if (length(function_name) == 1 && as.character(function_name) == "c" && length(res) > 1 && n_distinct(res) == 1 && res[[1]] %in% c("BOOL", "NUM", "STR")) {
-            str_c("c(", res[[1]], ")")
+        else if (length(function_name) == 1 && as.character(function_name) %in% c("c", "list") && length(res) > 1 && n_distinct(res) == 1 && res[[1]] %in% c("BOOL", "NUM", "STR", "VAR")) {
+            str_c(as.character(function_name), "(", res[[1]], ")")
         }
-        else if (length(function_name) == 1 && function_name == "::") {
-            str_c(as.character(function_args[[1]]), "::", as.character(function_args[[2]]))
+        else if (length(function_name) == 1 && function_name == "::") { # we drop the namespace
+            as.character(function_args[[2]])
         }
         else {
             function_name <- if (length(function_name) == 1) {
-                as.character(function_name)
+                func_name <- as.character(function_name)
+                function_name <- if (func_name %in% arith_op) {
+                    "OP"
+                }
+                else if (func_name %in% comp_op) {
+                    "COMP"
+                }
+                else if (func_name %in% bool_op) {
+                    "LOGI"
+                }
+                else {
+                    func_name
+                }
             }
             else {
-                canonic_expr(function_name)
+                canonic_expr(function_name, with.names)
             }
-            return(str_c(str_c(function_name, collapse = ", "), "(", str_c(res, collapse = ", "), ")"))
+
+            # It might have been transformed by canonic_expr in the recursive call above
+            # it should always have length one
+            res <- if (length(function_name) == 1) {
+                func_name <- as.character(function_name)
+                # VAR is absorbing: if there is at least one, we propagate to all of them
+                res <- if (func_name %in% c("COMP", "LOGI", "OP") && "VAR" %in% res) {
+                    rep.int("VAR", length(res))
+                }
+                else if (func_name == "model.frame") { # special case
+                    # We keep the 1st two arguments (usually formula and data)
+                    # and subset if it is there
+                    subset_arg <- if ("subset" %in% names(res)) {
+                        subset_s <- as.character(res["subset"])
+                        res["subset"] <- NULL
+                        str_c("subset = ", subset_s)
+                    }
+                    else {
+                        "subset = NULL"
+                    }
+                    first_args <- if(length(res) > 1) {
+                        res[1:2]
+                    }
+                    else {
+                        c(res[[1]], "NULL")
+                    }
+                    c(first_args, subset_arg)
+                }
+                else {
+                    res
+                }
+            }
+
+            fun_args <- if(with.names) {
+                str_c(names(res), if_else(names(res) == "", "",  " = "), res, collapse = ", ")
+            }
+            else
+            {
+                str_c(res, collapse = ", ")
+            }
+
+            return(str_c(str_c(function_name, collapse = ", "), "(", fun_args, ")"))
         }
     }
     else if (is.symbol(exp)) {
         return("VAR")
     }
     else if (is.expression(exp)) {
-        return(str_c(map(exp, canonic_expr), collapse = ", "))
+        return(str_c(map(exp, canonic_expr, with.names), collapse = ", "))
     }
     else if (typeof(exp) %in% c("integer", "double", "complex")) {
         return("NUM")
@@ -81,16 +134,21 @@ canonic_expr <- function(exp, keep.names = FALSE) {
     }
 }
 
-canonic_expr_str <- function(exp) {
+canonic_expr_str <- function(exp, with.names = FALSE) {
     ast <- NA
     try(ast <- parse(text = exp)[[1]], silent = TRUE)
     # some expr_resolved have been truncated so we mark them as FALSE (even though they could be true)
     if (is.symbol(ast) || is.language(ast) || length(ast) > 1 || !is.na(ast)) {
-        return(canonic_expr(ast))
+        return(canonic_expr(ast, with.names))
     }
     else {
         return("NORMALIZATION ERROR")
     }
+}
+
+simplify_digits <- function(expr_resolved) {
+    # Only digits?
+    str_replace_all(expr_resolved, "(?:c|list)\\(((?:\\d*(\\.\\d*)?,\\s)*\\d*(\\.\\d*)?)\\)", replacement = "c(1)")
 }
 
 
@@ -104,6 +162,10 @@ parse_program_arguments <- function() {
         make_option(
             c("--out-expr"),
             dest = "normalized_expr", metavar = "FILE"
+        ),
+        make_option(
+            c("--keep-names"),
+            action = "store_true", dest = "keep_names", default = FALSE,
         )
     )
     opt_parser <- OptionParser(option_list = option_list)
@@ -137,7 +199,7 @@ main <- function() {
     now <- Sys.time()
     cat("Normalize \n")
     expressions <- expressions %>%
-        mutate(expr_canonic = map_chr(expr_resolved, canonic_expr_str)) # , .keep = "unused"
+        mutate(expr_canonic = map_chr(expr_resolved, canonic_expr_str, with.names = arguments$keep_names)) # , .keep = "unused"
     res <- difftime(Sys.time(), now)
     cat("Done in ", res, units(res), "\n")
 
