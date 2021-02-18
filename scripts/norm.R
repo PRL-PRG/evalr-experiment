18be9614fa267ca82020bc3851bbcf3f43a283b5
Jan Vitek <janvite@yahoo.com>
2021-02-18 15:21:18 +0100

    norm

---
+++ scripts/norm.R
@@ -0,0 +1,35 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages(library(dplyr))
+#suppressPackageStartupMessages(library(purrr))
+#suppressPackageStartupMessages(library(tidyr))
+#library(readr)
+library(fst)
+library(optparse)
+library(evil)
+
+args <- parse_args(OptionParser(option_list = list(
+    make_option(c("--f"), dest="file", metavar="FILE"))))
+
+read_fst(args$file) %>% tibble() %>% select(-file) %>% unique() -> df
+
+# <pointer> and similar do not parse, wrap'em in strings
+r <- df$expr_resolved
+r <- gsub("<pointe[^>]*>", "\"<POINTER>\"", r, perl=T, useBytes=T)
+r <- gsub("<environment[^>]*>", "\"<ENVIRONMENT>\"", r, perl=T, useBytes=T)
+r <- gsub("<weak reference>", "\"<WEAK REFERENCE>\"", r, fixed=T, useBytes= T)
+
+normalize_it <- function(i) {
+  e <- r[i]
+  ast <- NA
+  if(is.null(e)) stop("parse() can't deal with NULL")
+  try(ast <- parse(text = e), silent = TRUE)
+  if (!is.expression(ast))  ast <- parse(text = "X")
+  # Parsing fails, when the expression is an operator such as +, semantically
+  # this a lookup of the symbol '+', for our purposes we can replace with X.
+  normalize(df$hash[i], ast)
+}
+
+sapply(1:length(r), normalize_it, USE.NAMES = F) -> ignore
+
+
